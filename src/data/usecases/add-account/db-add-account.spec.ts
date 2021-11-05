b77441e1c24b357b9c9a8241da1d2953b61ea056
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-05 19:38:52 -0300

    test: ensure DbAddAccount returns account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -84,4 +84,20 @@ describe('DbAddAccount', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = makeFakeAccountData()
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      email: 'any_email',
+      password: 'hashed_password',
+      name: 'any_name',
+      doc: 'any_doc',
+      about: 'any_about',
+      site: 'any_site',
+      role: 'any_role'
+    })
+  })
 })
