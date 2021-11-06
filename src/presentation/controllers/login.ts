e536ecf056e601486ea20cd0e0514a205b011ba7
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-06 16:06:03 -0300

    feat: ensure LoginController calls authentication with correct values

--- src/presentation/controllers/login.ts
+++ src/presentation/controllers/login.ts
@@ -2,12 +2,15 @@ import { badRequest, ok, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { BodyValidator } from '../protocols/body-validator'
+import { Authentication } from '../../domain/usecases/authentication'
 
 export class LoginController implements Controller {
   private readonly loginBodyValidator: BodyValidator
+  private readonly authentication: Authentication
 
-  constructor (loginBodyValidator: BodyValidator) {
+  constructor (loginBodyValidator: BodyValidator, authentication: Authentication) {
     this.loginBodyValidator = loginBodyValidator
+    this.authentication = authentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -15,6 +18,9 @@ export class LoginController implements Controller {
       const { body } = httpRequest
       const { error } = this.loginBodyValidator.isValid(body)
       if (error) return badRequest(error)
+
+      const { email, password } = body
+      await this.authentication.auth(email, password)
 
       return ok('ok')
     } catch (error) {
