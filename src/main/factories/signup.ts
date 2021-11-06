cb8effcfb853d1e418c678f7a4759558fa5c5aea
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-06 18:48:08 -0300

    refactor: fix signup controller folder name

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '.prisma/client'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountPostgresRepository } from '../../infra/db/postgresql/account-repository/account'
-import { SignUpController } from '../../presentation/controllers/singup/signup'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { SignUpValidator } from '../../utils/validators/signup-request-body'
 
 export const makeSignUpController = (): SignUpController => {
