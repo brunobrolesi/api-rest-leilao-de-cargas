f052ff904fe8170115c76ca0bd15c9e44b7b982c
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-06 18:50:53 -0300

    refactor: move bcrypt adapter files to a folder

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '.prisma/client'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountPostgresRepository } from '../../infra/db/postgresql/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { SignUpValidator } from '../../utils/validators/signup-request-body'
