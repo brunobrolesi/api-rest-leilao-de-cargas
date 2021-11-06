09125586b436b84174bef04fe2a268b86475de77
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-06 15:20:37 -0300

    test: ensure signup endpoint returns the correct values

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,19 +1,49 @@
 import request from 'supertest'
 import app from '../config/app'
+import prisma from '../../../client'
 
 describe('SignUp Routes', () => {
-  it('Should return an account on success', async () => {
-    await request(app)
+  beforeAll(async () => {
+    const deleteCustomers = prisma.customer.deleteMany()
+    const deleteProviders = prisma.provider.deleteMany()
+
+    await prisma.$transaction([deleteCustomers, deleteProviders])
+
+    await prisma.$disconnect()
+  })
+
+  it('Should return 400 and error message when invalid body is provided', async () => {
+    const response = await request(app)
       .post('/signup')
       .send({
-        email: 'valid_email',
+        email: 'invalid',
+        password: 'valid_password'
+      })
+      .expect(400)
+
+    expect(response.body.message).toBeTruthy()
+  })
+
+  it('Should return 201 and a account on success', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        email: 'email@mail.com',
         password: 'valid_password',
         name: 'valid_name',
-        doc: 'valid_doc',
+        doc: '60.429.484/0001-10',
         about: 'valid_about',
         site: 'valid_site',
-        role: 'valid_role'
+        role: 'customer'
       })
-      .expect(200)
+      .expect(201)
+
+    expect(response.body.id).toBeTruthy()
+    expect(response.body.email).toBe('email@mail.com')
+    expect(response.body.password).not.toBe('valid_password')
+    expect(response.body.name).toBe('valid_name')
+    expect(response.body.doc).toBe('60.429.484/0001-10')
+    expect(response.body.about).toBe('valid_about')
+    expect(response.body.site).toBe('valid_site')
   })
 })
