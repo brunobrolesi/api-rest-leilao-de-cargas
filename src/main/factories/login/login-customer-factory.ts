f8ffdafee917bbf90a588b831a00a53cda6c36f1
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-06 23:29:55 -0300

    feat: add login customer factory

---
+++ src/main/factories/login/login-customer-factory.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '.prisma/client'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountCustomerPostgresRepository } from '../../../infra/db/postgresql/account-customer/account-customer-repository'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { Controller } from '../../../presentation/protocols/controller'
+import { LoginValidator } from '../../../utils/validators/login/login-request-body'
+import { env } from '../../config/env'
+
+export const makeCustomerSignUpController = (): Controller => {
+  const prisma = new PrismaClient()
+  const loadAccountRepository = new AccountCustomerPostgresRepository(prisma)
+  const salt = 12
+  const hashComparer = new BcryptAdapter(salt)
+  const tokenGenerator = new JwtAdapter(env.jwtSecret)
+  const loginBodyValidator = new LoginValidator()
+  const authentication = new DbAuthentication(loadAccountRepository, hashComparer, tokenGenerator)
+  return new LoginController(loginBodyValidator, authentication)
+}
