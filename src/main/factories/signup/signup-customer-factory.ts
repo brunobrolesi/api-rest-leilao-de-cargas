e6067eb12c1844352ad87e7b0d70a92a0cc52c6c
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-06 23:28:34 -0300

    refactor: change factory return

--- src/main/factories/signup/signup-customer-factory.ts
+++ src/main/factories/signup/signup-customer-factory.ts
@@ -3,9 +3,10 @@ import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountCustomerPostgresRepository } from '../../../infra/db/postgresql/account-customer/account-customer-repository'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
-import { SignUpValidator } from '../../../utils/validators/signup-request-body'
+import { Controller } from '../../../presentation/protocols/controller'
+import { SignUpValidator } from '../../../utils/validators/signup/signup-request-body'
 
-export const makeCustomerSignUpController = (): SignUpController => {
+export const makeCustomerSignUpController = (): Controller => {
   const salt = 12
   const encrypter = new BcryptAdapter(salt)
   const prima = new PrismaClient()
--- src/main/factories/signup/signup-provider-factory.ts
+++ src/main/factories/signup/signup-provider-factory.ts
@@ -3,9 +3,10 @@ import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountProviderPostgresRepository } from '../../../infra/db/postgresql/account-provider/account-provider-repository'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
-import { SignUpValidator } from '../../../utils/validators/signup-request-body'
+import { Controller } from '../../../presentation/protocols/controller'
+import { SignUpValidator } from '../../../utils/validators/signup/signup-request-body'
 
-export const makeProviderSignUpController = (): SignUpController => {
+export const makeProviderSignUpController = (): Controller => {
   const salt = 12
   const encrypter = new BcryptAdapter(salt)
   const prima = new PrismaClient()
