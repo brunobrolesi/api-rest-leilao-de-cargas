36a7bb587f38962abb3ea2d9cae6c0d8fd9e2003
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-08 17:51:46 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Teste BackEnd GoFlux',
+    description: 'API REST capaz de cadastrar e obter informações de Embarcadores, Transportadores, Ofertas e Lances',
+    version: '1.0.0'
+  }
+}
