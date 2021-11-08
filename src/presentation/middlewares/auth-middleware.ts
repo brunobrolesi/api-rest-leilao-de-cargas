947fd5f3827610f2fc8db20df8506c6906ec6968
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-08 00:45:49 -0300

    feat: ensure AuthMiddleware call token verifier with correct token

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,13 +1,18 @@
+import { TokenVerifier } from '../../data/protocols/criptography/token-verifier'
 import { MissingAuthToken } from '../errors/missing-auth-token'
 import { forbidden, ok } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly tokenVerifier: TokenVerifier) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const token = httpRequest.headers?.authorization
 
     if (!token) return forbidden(new MissingAuthToken())
+
+    await this.tokenVerifier.verify(token)
 
     return ok(null)
   }
