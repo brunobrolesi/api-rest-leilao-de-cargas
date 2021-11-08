dc6f2808b0ed920841761d800b30742ed58b2239
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-08 12:06:25 -0300

    feat: ensure OfferRepository loadById Behaves as expected

--- src/infra/db/postgresql/offer/offer-repository.ts
+++ src/infra/db/postgresql/offer/offer-repository.ts
@@ -1,9 +1,10 @@
 import { PrismaClient, Prisma, AmountType } from '.prisma/client'
 import { AddOfferRepository } from '../../../../data/protocols/db/add-offer-repository'
+import { LoadOfferByIdRepository } from '../../../../data/protocols/db/load-offer-by-id-repository'
 import { OfferModel } from '../../../../domain/models/offer'
 import { AddOfferModel } from '../../../../domain/usecases/add-offer'
 
-export class OfferPostgresRepository implements AddOfferRepository {
+export class OfferPostgresRepository implements AddOfferRepository, LoadOfferByIdRepository {
   constructor (private readonly prisma: PrismaClient) {}
 
   async add (offerData: AddOfferModel): Promise<OfferModel> {
@@ -11,8 +12,12 @@ export class OfferPostgresRepository implements AddOfferRepository {
     const decimal_initial_value = new Prisma.Decimal(initial_value)
     const decimal_amount = new Prisma.Decimal(amount)
     const typeAsAmountType = amount_type as AmountType
-    const OfferWithDecimalValues = Object.assign({}, offerData, { initial_value: decimal_initial_value, amount: decimal_amount, amount_type: typeAsAmountType })
-    const offer = await this.prisma.offer.create({ data: OfferWithDecimalValues })
-    return { id: offer.id, ...offerData }
+    const offerWithDecimalValues = Object.assign({}, offerData, { initial_value: decimal_initial_value, amount: decimal_amount, amount_type: typeAsAmountType })
+    const offer = await this.prisma.offer.create({ data: offerWithDecimalValues })
+    return offer as unknown as OfferModel
+  }
+
+  async loadById (id: number): Promise<OfferModel|null> {
+    return this.prisma.offer.findUnique({ where: { id } }) as unknown as OfferModel
   }
 }
