f8371c75cd9cd3980d7d9bd63eb8080de22e0d35
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-08 09:00:14 -0300

    feat: ensure jwtAdapter implements token verifier and behaves as expected

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,7 +1,8 @@
 import jwt, { SignOptions } from 'jsonwebtoken'
 import { TokenData, TokenGenerator } from '../../../data/protocols/criptography/token-generator'
+import { TokenPayload, TokenVerifier } from '../../../data/protocols/criptography/token-verifier'
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, TokenVerifier {
   constructor (private readonly secret: string) {}
 
   generate (data: TokenData): string {
@@ -11,5 +12,10 @@ export class JwtAdapter implements TokenGenerator {
     }
 
     return jwt.sign(data, this.secret, jwtConfig)
+  }
+
+  verify (token: string): TokenPayload {
+    const payload = jwt.verify(token, this.secret)
+    return payload as TokenPayload
   }
 }
