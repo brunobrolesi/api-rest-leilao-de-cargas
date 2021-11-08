f89835740820ec966a167f6dae0c6dcdd3678bc8
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-08 00:55:57 -0300

    feat: ensure AuthMiddleware returns 401 if role is unauthorized

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,18 +1,23 @@
 import { TokenVerifier } from '../../data/protocols/criptography/token-verifier'
 import { MissingAuthToken } from '../errors/missing-auth-token'
-import { forbidden, ok } from '../helpers/http-helper'
+import { forbidden, ok, unauthorized } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
-  constructor (private readonly tokenVerifier: TokenVerifier) {}
+  constructor (
+    private readonly tokenVerifier: TokenVerifier,
+    private readonly role: string
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const token = httpRequest.headers?.authorization
 
     if (!token) return forbidden(new MissingAuthToken())
 
-    await this.tokenVerifier.verify(token)
+    const { role } = await this.tokenVerifier.verify(token)
+
+    if (this.role !== role) return unauthorized()
 
     return ok(null)
   }
