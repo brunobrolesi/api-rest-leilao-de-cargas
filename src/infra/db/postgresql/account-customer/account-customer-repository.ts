7fe5facfd10b0f7539a1160231b6af0285192413
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-08 16:42:18 -0300

    feat: add loadAll method to AccountRepository

--- src/infra/db/postgresql/account-customer/account-customer-repository.ts
+++ src/infra/db/postgresql/account-customer/account-customer-repository.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '.prisma/client'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
-import { AccountModel } from '../../../../domain/models/account'
+import { LoadAllAccountsRepository } from '../../../../data/protocols/db/load-all-accounts-repository'
+import { AccountModel, ResumedAccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 
-export class AccountCustomerPostgresRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountCustomerPostgresRepository implements AddAccountRepository, LoadAccountByEmailRepository, LoadAllAccountsRepository {
   private readonly prisma: PrismaClient
 
   constructor (prisma: PrismaClient) {
@@ -19,5 +20,18 @@ export class AccountCustomerPostgresRepository implements AddAccountRepository, 
 
   async loadByEmail (email: string): Promise<AccountModel|null> {
     return await this.prisma.customer.findUnique({ where: { email } })
+  }
+
+  async loadAll (): Promise<ResumedAccountModel[]|[]> {
+    return await this.prisma.customer.findMany({
+      where: {
+        active: true
+      },
+      select: {
+        id: true,
+        name: true,
+        doc: true
+      }
+    })
   }
 }
--- src/infra/db/postgresql/account-provider/account-provider-repository.ts
+++ src/infra/db/postgresql/account-provider/account-provider-repository.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '.prisma/client'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
-import { AccountModel } from '../../../../domain/models/account'
+import { LoadAllAccountsRepository } from '../../../../data/protocols/db/load-all-accounts-repository'
+import { AccountModel, ResumedAccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 
-export class AccountProviderPostgresRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountProviderPostgresRepository implements AddAccountRepository, LoadAccountByEmailRepository, LoadAllAccountsRepository {
   constructor (private readonly prisma: PrismaClient) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
@@ -15,5 +16,18 @@ export class AccountProviderPostgresRepository implements AddAccountRepository, 
 
   async loadByEmail (email: string): Promise<AccountModel|null> {
     return await this.prisma.provider.findUnique({ where: { email } })
+  }
+
+  async loadAll (): Promise<ResumedAccountModel[]|[]> {
+    return await this.prisma.provider.findMany({
+      where: {
+        active: true
+      },
+      select: {
+        id: true,
+        name: true,
+        doc: true
+      }
+    })
   }
 }
