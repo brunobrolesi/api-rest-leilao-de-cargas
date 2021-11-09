47c95c9bf7deaeebfadd75272280479e5a58f6ca
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-09 10:09:43 -0300

    refactor: remove writed secret when .env loads fail

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  port: process.env.PORT ?? 5050,
-  jwtSecret: process.env.JWT_SECRET ?? 'my_secret'
+  port: process.env.SERVER_PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET
 }
